90bbbbda744237f7d0a964b1874b43bf500b42de
unknown <Munna@personal-80df87.(none)>
2013-03-21 05:04:28 +0600

    Completed

--- items.php
+++ items.php
@@ -4,7 +4,171 @@
     <div class="row-fluid">
         <div class="span9">
             <div class="row-fluid">
-                
+                <div class="span12">
+                    <!-- rounded menus' -->
+                    <div class="row-fluid">
+                        <div class="span2">
+                            <a href="#" class="badge badge-warning">Add to Cart</a>
+                        </div>
+                        <div class="span1"></div>
+                        <div class="span2">
+                            <a href="#" class="badge badge-warning">Pricing Plan</a>
+                        </div>
+                        <div class="span1"></div>
+                        <div class="span2">
+                            <a href="#" class="badge badge-warning">Payment</a>
+                        </div>
+                        <div class="span1"></div>
+                        <div class="span2">
+                            <a href="#" class="badge badge-warning">Complete</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="span12"></div>
+                <div class="span12">
+                    <!-- Search butttons -->
+                    <form>
+                        <div class="row-fluid">
+                            <div class="span3">
+                                <select>
+                                    <option>Top Paid</option>
+                                    <option>Low Paid</option>
+                                </select>
+                            </div>
+                            <div class="span1"></div>
+                            <div class="span3">
+                                <select>
+                                    <option>Romance</option>
+                                    <option>Horror</option>
+                                </select>
+                            </div>
+                            <div class="span1"></div>
+                            <div class="span2">
+                                <div class="control-group">                                    
+                                    <div class="controls">
+                                        <div class="input-prepend">
+                                            <button type="submit" class="btn"><i class="icon-search"></i></button>
+                                            <input type="text" class="ssearch-query" placeholder="search">
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+                <div class="span12" style="overflow: scroll;height: 300px;">
+                    <!-- tables -->
+                    <table class="table">
+                        <tr>
+                        <div class="row-fluid">                            
+                            <div class="span3">
+                                <div class="row-fluid img-polaroid">
+                                    <div class="span12">
+                                        <input type="checkbox" class="pull-right"/>
+                                        <img src="assets/images/homepage.jpg"/>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="span3">
+                                <div class="row-fluid img-polaroid">
+                                    <div class="span12">
+                                        <input type="checkbox" class="pull-right"/>
+                                        <img src="assets/images/homepage.jpg"/>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="span3">
+                                <div class="row-fluid img-polaroid">
+                                    <div class="span12">
+                                        <input type="checkbox" class="pull-right"/>
+                                        <img src="assets/images/homepage.jpg"/>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="span3">
+                                <div class="row-fluid img-polaroid">
+                                    <div class="span12">
+                                        <input type="checkbox" class="pull-right"/>
+                                        <img src="assets/images/homepage.jpg"/>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        </tr>
+                        <tr>
+                        <div class="row-fluid">
+                            <div class="span3">
+                                <div class="row-fluid img-polaroid">
+                                    <div class="span12">
+                                        <input type="checkbox" class="pull-right"/>
+                                        <img src="assets/images/homepage.jpg"/>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="span3">
+                                <div class="row-fluid img-polaroid">
+                                    <div class="span12">
+                                        <input type="checkbox" class="pull-right"/>
+                                        <img src="assets/images/homepage.jpg"/>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="span3">
+                                <div class="row-fluid img-polaroid">
+                                    <div class="span12">
+                                        <input type="checkbox" class="pull-right"/>
+                                        <img src="assets/images/homepage.jpg"/>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="span3">
+                                <div class="row-fluid img-polaroid">
+                                    <div class="span12">
+                                        <input type="checkbox" class="pull-right"/>
+                                        <img src="assets/images/homepage.jpg"/>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        </tr>
+                        <tr>
+                        <div class="row-fluid">
+                            <div class="span3">
+                                <div class="row-fluid img-polaroid">
+                                    <div class="span12">
+                                        <input type="checkbox" class="pull-right"/>
+                                        <img src="assets/images/homepage.jpg"/>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="span3">
+                                <div class="row-fluid img-polaroid">
+                                    <div class="span12">
+                                        <input type="checkbox" class="pull-right"/>
+                                        <img src="assets/images/homepage.jpg"/>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="span3">
+                                <div class="row-fluid img-polaroid">
+                                    <div class="span12">
+                                        <input type="checkbox" class="pull-right"/>
+                                        <img src="assets/images/homepage.jpg"/>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="span3">
+                                <div class="row-fluid img-polaroid">
+                                    <div class="span12">
+                                        <input type="checkbox" class="pull-right"/>
+                                        <img src="assets/images/homepage.jpg"/>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        </tr>
+                    </table>
+                </div>
             </div>
         </div>
         <div class="span3">
